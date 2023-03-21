27f5ca1a63282af9cfd7e3caa527d26fca8c45fc
amir asghari <amirdev2001@gmail.com>
2023-03-21 19:57:53 +0330

    wrote the verboos names

--- activity/models.py
+++ activity/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 from lib.common_models import BaseModel
 from django.contrib.auth import get_user_model 
 from content.models import Post
@@ -15,6 +15,11 @@ class Comment(BaseModel):
     def __str__(self):
         return self.caption
     
+    class Meta:
+        verbose_name = _('comment')
+        verbose_name_plural = _('comments')
+
+    
 class Like(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
@@ -22,7 +27,10 @@ class Like(BaseModel):
     def __str__(self):
         return "{} >> {}".format(self.user.username, self.post.id)
     
-    
+
+    class Meta:
+        verbose_name = _('like')
+        verbose_name_plural = _('likes')
 
                              
 
